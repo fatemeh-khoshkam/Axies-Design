c60b8451b9269245700f873b86ce256e684e43b9
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-04-23 01:35:02 +0430

    add profile page

--- js/script.js
+++ js/script.js
@@ -7,33 +7,6 @@ window.onresize = function () {
    }
 };
 
-const menu = document.getElementById('header');
-const scroll_btn = document.getElementById("scroll--top");
-
-scroll_btn.addEventListener("click", () => {
-  window.scrollTo(0, 0);
-});
-
-window.addEventListener("scroll", () => {
-   scroll_btn.classList.add("scroll--top--show");
-
-  let scrollTop = window.pageYOffset;
-
-  if (scrollTop === 0) {
-   scroll_btn.classList.remove("scroll--top--show");
-  }
-});
- 
- window.addEventListener("scroll", function () {
-    
-   let scroll_Y = window.scrollY;
-   if (scroll_Y >= 75) {
-      menu.classList.add("sticky--header");
-   } else {
-      menu.classList.remove("sticky--header");
-   }
- });
-
 
  // Set the date we're counting down to
 function setTimer(enddate, elem){
