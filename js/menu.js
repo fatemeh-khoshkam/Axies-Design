81d90904d7e9a0e0407a9b053c25f7d42737f536
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-10 21:07:57 +0430

    update menu

--- js/menu.js
+++ js/menu.js
@@ -6,21 +6,21 @@ scroll_btn.addEventListener("click", () => {
 });
 
 window.addEventListener("scroll", () => {
-   scroll_btn.classList.add("scroll--top--show");
+  scroll_btn.classList.add("scroll--top--show");
 
   let scrollTop = window.pageYOffset;
 
   if (scrollTop === 0) {
-   scroll_btn.classList.remove("scroll--top--show");
+    scroll_btn.classList.remove("scroll--top--show");
   }
 });
- 
- window.addEventListener("scroll", function () {
-    
-   let scroll_Y = window.scrollY;
-   if (scroll_Y >= 75) {
-      menu.classList.add("sticky--header");
-   } else {
-      menu.classList.remove("sticky--header");
-   }
- });
+
+window.addEventListener("scroll", function () {
+  
+  let scroll_Y = window.scrollY;
+  if (scroll_Y >= 75) {
+    menu.classList.add("sticky--header");
+  } else {
+    menu.classList.remove("sticky--header");
+  }
+});
