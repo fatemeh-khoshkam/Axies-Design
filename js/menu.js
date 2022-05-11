bbab22eab28a7a67030a835e079f376b202796a5
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-11 20:25:06 +0430

    add slider

--- js/menu.js
+++ js/menu.js
@@ -129,3 +129,24 @@ window.addEventListener('resize',() => {
   checkMediaQuery();
   chkWallet();    
 });
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:20,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      900:{
+        items:3
+      },
+      1000:{
+          items:4
+      }
+  } 
+})
