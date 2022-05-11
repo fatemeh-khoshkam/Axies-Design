4b74689d426645ee55199903819ddfe57f06a916
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-12 00:04:03 +0430

    update item details page

--- js/script.js
+++ js/script.js
@@ -19,23 +19,6 @@ function setTimer(enddate, elem){
   })()
 }
 
-setTimer(
-   new Date("May 21, 2022 18:17:04"),
-   document.getElementById("card__time1")
-  );
- setTimer(
-   new Date("July 29, 2022 15:37:25"),
-   document.getElementById("card__time2")
-  );
-  setTimer(
-   new Date("Septamber 25, 2022 05:30:55"),
-   document.getElementById("card__time3")
-  );
-  setTimer(
-   new Date("June 23, 2022 22:19:45"),
-   document.getElementById("card__time4")
-  );
-
 
 
 
